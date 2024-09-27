a9222e2c5530cb6a5c3dcecd3f80a897fe6fb3a5
R3pic <101osc@naver.com>
2024-09-27 22:39:37 +0900

    feat: 0.0.2

--- APIUpdate.ts
+++ APIUpdate.ts
@@ -3,24 +3,15 @@ import { Env } from "./src/core/Env";
 import type { BotCommand } from "./src/core/BotCommand";
 import { ExampleCommand } from "./src/ExampleService/PingPongCommand";
 
+console.info(`Bot Token : ${Env.BOT_TOKEN}`);
+console.info(`Bot ID : ${Env.BOT_ID}`);
+
 const rest = new REST().setToken(Env.BOT_TOKEN);
 
-class CommandLoader {
-    static load(commands: BotCommand[]) {
-        const loaded: RESTPostAPIChatInputApplicationCommandsJSONBody[] = [];
-        commands.forEach(command => {
-            const commandData = command.toJSON() as RESTPostAPIChatInputApplicationCommandsJSONBody;
-            loaded.push(commandData);
-        });
-
-        return loaded;
-    }
-}
-
-const commands = CommandLoader.load([
+const commands = load(
     /* Command for APIUpdate */
     new ExampleCommand(),
-]);
+);
 
 console.info('Command API update Start');
 try {
@@ -29,9 +20,11 @@ try {
     const currentCommands: APIApplicationCommand[] = await rest.get(
         Routes.applicationCommands(Env.BOT_ID)
     ) as APIApplicationCommand[];
+
     const commandsToDelete = currentCommands.filter(currentCommand => {
         return !commands.some(newCommand => newCommand.name === currentCommand.name);
     });
+
     for (const command of commandsToDelete) {
         await rest.delete(
             `${Routes.applicationCommands(Env.BOT_ID)}/${command.id}`
@@ -45,7 +38,19 @@ try {
     );
 
     console.debug('Successfully reloaded application (/) commands.');
-} catch (error) {
-    console.error(error);
+} catch (err) {
+    if (err instanceof Error) {
+        console.error(err);
+    }
 }
 console.info('Command API update End');
+
+function load(...commands: BotCommand[]) {
+    const loaded: RESTPostAPIChatInputApplicationCommandsJSONBody[] = [];
+    commands.forEach(command => {
+        const commandData = command.toJSON() as RESTPostAPIChatInputApplicationCommandsJSONBody;
+        loaded.push(commandData);
+    });
+
+    return loaded;
+}
